dd6258c27e29ad664eaf96bdd7418e7aa18e0082
ace3366 <poohmandu22@gmail.com>
2024-03-21 15:56:37 +0700

    Updates

---
+++ precache-manifest.28e275401993ee916430bc0c566012a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78da4a7d401e74e5fdcc12b7be62572",
+    "url": "/ace3366.github.io/phimmoi/index.html"
+  },
+  {
+    "revision": "53e8f9ea466127562178",
+    "url": "/ace3366.github.io/phimmoi/static/css/main.bc0b07fb.chunk.css"
+  },
+  {
+    "revision": "ccb4ad7fbace550e3941",
+    "url": "/ace3366.github.io/phimmoi/static/js/2.9edbe9cf.chunk.js"
+  },
+  {
+    "revision": "d5291a34dddf3ac93da2c7750a0c7b8e",
+    "url": "/ace3366.github.io/phimmoi/static/js/2.9edbe9cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e8f9ea466127562178",
+    "url": "/ace3366.github.io/phimmoi/static/js/main.4d1a6ae6.chunk.js"
+  },
+  {
+    "revision": "ab5a9e1756e805076769",
+    "url": "/ace3366.github.io/phimmoi/static/js/runtime-main.e6c3b5d3.js"
+  }
+]);
